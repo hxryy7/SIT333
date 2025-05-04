46407d524883bffbc5a6e8bc3478754d0540e6db
Harry_07 <128872332+hxryy7@users.noreply.github.com>
2025-05-04 12:30:52 +1000

    Add files via upload

--- task5_1P/src/main/java/sit707_week5/WeatherController.java
+++ task5_1P/src/main/java/sit707_week5/WeatherController.java
@@ -10,11 +10,9 @@ public class WeatherController {
 	public static final int HOURS_PER_DAY = 3;
 
 	private static WeatherController instance;
+
+	private static double[] todaysHourlyTemperature = new double[HOURS_PER_DAY];
 	
-	/**
-	 * Factory method for single instance WeatherController.
-	 * @return
-	 */
 	public static WeatherController getInstance() {
 		if (instance == null) {
 			instance = new WeatherController();
@@ -22,22 +20,13 @@ public class WeatherController {
 		return instance;
 	}
 	
-	// Initialise 10 hourly temperature
-	private static double[] todaysHourlyTemperature = new double[HOURS_PER_DAY];
-	
-	/**
-	 * Private constructor prevents to create new instance manually.
-	 * A factory method needs to be used.
-	 */
 	private WeatherController() {
 		System.out.println("Creating new weather controller.");
-		
-		// sleep a while to simulate a delay
-		sleep(2 + new Random().nextInt(5));
-		
-		// Insert 10 random temperature values in today's hourly list.
+
+		sleep(1);
+
 		Random random = new Random();
-		for (int i=0; i<HOURS_PER_DAY; i++) {
+		for (int i = 0; i < HOURS_PER_DAY; i++) {
 			todaysHourlyTemperature[i] = 1 + random.nextInt(30);
 		}
 		System.out.println(Arrays.toString(todaysHourlyTemperature));
@@ -46,103 +35,68 @@ public class WeatherController {
 	public void close() {
 		System.out.println("Closing weather controller.");
 		instance = null;
-		
-		// sleep a while to simulate a delay
-		sleep(2 + new Random().nextInt(5));
+		sleep(1);
 	}
 	
-	/**
-	 * Calculate minimum of today's hourly temperatures.
-	 * @return
-	 */
 	public double getTemperatureMinFromCache() {
 		double minVal = 1000;
-		for (int i=0; i<todaysHourlyTemperature.length; i++) {
-			if (minVal > todaysHourlyTemperature[i]) {
-				minVal = todaysHourlyTemperature[i];
+		for (double temp : todaysHourlyTemperature) {
+			if (minVal > temp) {
+				minVal = temp;
 			}
 		}
 		return minVal;
 	}
 
-	/**
-	 * Calculate maximum of today's hourly temperatures.
-	 * @return
-	 */
 	public double getTemperatureMaxFromCache() {
 		double maxVal = -1;
-		for (int i=0; i<todaysHourlyTemperature.length; i++) {
-			if (maxVal < todaysHourlyTemperature[i]) {
-				maxVal = todaysHourlyTemperature[i];
+		for (double temp : todaysHourlyTemperature) {
+			if (maxVal < temp) {
+				maxVal = temp;
 			}
 		}
 		return maxVal;
 	}
 	
-	/**
-	 * Calculate average of today's hourly temperatures.
-	 * @return
-	 */
 	public double getTemperatureAverageFromCache() {
 		double sumVal = 0;
-		for (int i=0; i<todaysHourlyTemperature.length; i++) {
-			sumVal += todaysHourlyTemperature[i];
+		for (double temp : todaysHourlyTemperature) {
+			sumVal += temp;
 		}
-		return sumVal/todaysHourlyTemperature.length;
+		return sumVal / todaysHourlyTemperature.length;
 	}
 	
-	/**
-	 * Return temperature for given hour of current day.
-	 * @param hour
-	 * @return
-	 */
 	public double getTemperatureForHour(int hour) {
-		// sleep a while to simulate a delay
-		sleep(1 + new Random().nextInt(5));
-		
-		// Let's return a randomly selected temperature from hourly list if hour does not exist.
+		sleep(1);
+
 		if (hour > todaysHourlyTemperature.length) {
 			hour = 1 + new Random().nextInt(todaysHourlyTemperature.length);
 		}
-		
-		// Hour index starts from 0 instead of 1 due to array indexing.
-		return todaysHourlyTemperature[hour-1];
+		return todaysHourlyTemperature[hour - 1];
 	}
 	
-	/**
-	 * Persist reported temperature to data store and return recorded time. 
-	 * @param hour
-	 * @param temperature
-	 * @return
-	 */
+	// New overloaded method for test injection
+	public String persistTemperature(int hour, double temperature, Date time) {
+	    SimpleDateFormat sdf = new SimpleDateFormat("H:m:s");
+	    String savedTime = sdf.format(time);
+	    System.out.println("Temperature: " + temperature + " of hour: " + hour + ", saved at " + savedTime);
+	    sleep(1);
+	    return savedTime;
+	}
+	
+	// Original method now calls the overloaded one
 	public String persistTemperature(int hour, double temperature) {
-		SimpleDateFormat sdf = new SimpleDateFormat("H:m:s");
-		String savedTime = sdf.format(new Date());
-		System.out.println("Temperature: " + temperature + " of hour: " + hour + ", saved at " + savedTime);
-		
-		// sleep a while to simulate a delay
-		sleep(1 + new Random().nextInt(2));
-		
-		return savedTime;
+	    return persistTemperature(hour, temperature, new Date());
 	}
 	
-	/**
-	 * Calculated the number of hours temperature data is available for today.
-	 * @return
-	 */
 	public int getTotalHours() {
 		return todaysHourlyTemperature.length;
 	}
 	
-	/**
-	 * Sleep for specified seconds.
-	 * @param sec
-	 */
 	public static void sleep(int sec) {
 		try {
-			Thread.sleep(sec*1000);
+			Thread.sleep(sec * 100); // 100 ms = 0.1 sec
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
